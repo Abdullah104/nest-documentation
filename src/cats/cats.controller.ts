c71810d0241ab334fd51029582e0326c569dfcf8
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2025-11-06 11:52:06 +0300

    Overview/Controllers/Route Parameters

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,34 @@
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  Logger,
+  Param,
+  Post,
+  Req,
+} from '@nestjs/common';
+import type { HttpRedirectResponse } from '@nestjs/common';
+import type { Request } from 'express';
+
+@Controller({ path: 'cats' })
+export class CatsController {
+  @Get('ab{*splash}cd')
+  findAll(): HttpRedirectResponse {
+    return { url: 'http://localhost:8080', statusCode: 300 };
+  }
+
+  @Post()
+  @HttpCode(204)
+  @Header('Cache-Control', 'no-store')
+  create() {
+    return 'This action adds a cat';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number, @Req() request: Request) {
+    Logger.log(request.host);
+
+    return `This actions returns a #${id} cat`;
+  }
+}
