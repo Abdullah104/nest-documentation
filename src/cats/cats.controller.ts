ff49b72bb82793ef4ee7e602b04b5b9924de7540
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2025-11-07 18:24:19 +0300

    Overview/Providers

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -2,66 +2,32 @@ import {
   Body,
   Controller,
   Get,
-  Header,
-  HostParam,
-  Param,
+  Inject,
+  Logger,
+  Optional,
   Post,
-  Query,
 } from '@nestjs/common';
-import { Observable, of } from 'rxjs';
-import { CreateCatDto } from './create-cat.dto';
-
-class Cat {
-  constructor(
-    readonly id: number,
-    readonly name: string,
-    readonly age: number,
-    readonly breed: string,
-  ) {}
-}
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
+import { CreateCatDto } from './dto/create-cat.dto';
 
 @Controller({ path: 'cats' })
 export class CatsController {
-  cats: Cat[] = [];
+  @Optional()
+  @Inject('HTTP_OPTIONS')
+  private unexistingOptional: any;
 
-  @Get('ab{*splash}cd')
-  findAll(
-    @Query('filter[where][age]') age: number,
-    @Query('filter[where][breed]') breed: string,
-  ): Observable<Cat[]> {
-    const cats: Cat[] = [];
-    const stream: Observable<Cat[]> = of(cats);
+  constructor(private readonly catsService: CatsService) {}
 
-    for (const cat of this.cats) {
-      if (
-        (!age && !breed) ||
-        (age && cat.age == age) ||
-        (breed && cat.breed == breed)
-      ) {
-        cats.push(cat);
-      }
-    }
+  @Get()
+  findAll(): Cat[] {
+    Logger.log(this.unexistingOptional);
 
-    return stream;
+    return this.catsService.findAll();
   }
 
   @Post()
-  @Header('Cache-Control', 'no-store')
   create(@Body() createCatDto: CreateCatDto) {
-    const newCat = new Cat(
-      this.cats.length,
-      createCatDto.name,
-      createCatDto.age,
-      createCatDto.breed,
-    );
-
-    this.cats.push(newCat);
-
-    return newCat;
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: number, @HostParam('host') host: string) {
-    return `This actions returns a #${id} cat from host ${host}`;
+    this.catsService.create(createCatDto);
   }
 }
